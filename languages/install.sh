c246723310e432e9e13a85ad5876863fc70d6770
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-05 21:08:05 -0300

    feat: remove desnecessary code in languages/install.sh

--- languages/install.sh
+++ languages/install.sh
@@ -5,10 +5,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     export_all_systems=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1| cut -d' ' -f1);
 fi
 
-# env for get all systems for current linux
-
-export_all_systems=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1| cut -d' ' -f1);
-
 
 case $export_all_systems in
     *debian*)
