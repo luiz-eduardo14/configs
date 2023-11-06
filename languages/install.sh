e13ce82ba10b2e456d1af7699bb0f787cac3f6df
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-11-05 23:22:18 -0300

    feat: apply script export_all_systems

--- languages/install.sh
+++ languages/install.sh
@@ -1,9 +1,4 @@
-if [ "$machine" == "Mac" ]; then
-    export_all_systems="Mac"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # env for get all systems for current linux
-    export_all_systems=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1| cut -d' ' -f1);
-fi
+source ../utils/export_all_systems.sh
 
 
 case $export_all_systems in
